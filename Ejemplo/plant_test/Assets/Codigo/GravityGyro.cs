9760a6fe85c51c5220fd234e67d31f83668c6c7e
Saul <smi0010@alu.ubu.es>
2020-03-10 20:30:17 +0100

    Implementación del uso del giróscopio
    Implementación del uso del giróscopio del smartphone, para que las particulas se muevan acorde a la inclinación.

---
+++ Ejemplo/plant_test/Assets/Codigo/GravityGyro.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityGyro : MonoBehaviour
+{
+    
+    public ParticleSystem particulas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3();
+        Vector3 pos2 = new Vector3();
+        pos2.z = 0;
+        pos2.y = 0;
+        pos2.x = 10;
+        //particulas.gravityModifier =10;//pos.y;
+        Physics.gravity = Input.gyro.gravity;
+
+        //transform.position = pos;
+    }
+}

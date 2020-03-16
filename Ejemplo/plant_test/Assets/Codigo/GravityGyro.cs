3e37f8082165fc10cdc0ee5fee29ccde7ddfac36
Saul <smi0010@alu.ubu.es>
2020-03-17 00:09:46 +0100

    Solución bug giróscopio
    Un paramentro hacia que el eje de las fisicas no coicidiría con el de gravedad.
    Ademas en determinada posicion, hay que invertir el eje Z.

--- Ejemplo/plant_test/Assets/Codigo/GravityGyro.cs
+++ Ejemplo/plant_test/Assets/Codigo/GravityGyro.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GravityGyro : MonoBehaviour
 {
-    
     public ParticleSystem particulas;
+    //public Text ejes;
     // Start is called before the first frame update
+    /*public int x;
+    public int y;
+    public int z;*/
     void Start()
     {
         if (SystemInfo.supportsGyroscope)
@@ -20,13 +24,20 @@ public class GravityGyro : MonoBehaviour
     void Update()
     {
         Vector3 pos = new Vector3();
-        Vector3 pos2 = new Vector3();
-        pos2.z = 0;
-        pos2.y = 0;
-        pos2.x = 10;
-        //particulas.gravityModifier =10;//pos.y;
-        Physics.gravity = Input.gyro.gravity;
-
-        //transform.position = pos;
+        /*Vector3 pos2 = new Vector3();
+        pos2.z = x;
+        pos2.y = y;
+        pos2.x = z;*/
+        /*
+         * //Este testo es para comprobar las cordenadas dados por el giroscopio
+        ejes.text = "El eje X =" + Input.gyro.gravity.x + " " +
+            "El eje Y =" + Input.gyro.gravity.y + " " +
+            "El eje Z " + Input.gyro.gravity.z;
+        pos = Input.gyro.gravity;*/
+        //Cuando la camara mira hacia abajo,invierte el eje Z.
+        if (Input.gyro.gravity.z < 0)
+            pos.z = pos.z * (-1);
+        Physics.gravity = pos;
+        
     }
 }

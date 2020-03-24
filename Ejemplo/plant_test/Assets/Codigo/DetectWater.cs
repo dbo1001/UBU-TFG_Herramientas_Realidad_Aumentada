c29af08a38e3bde0282dc2d40a43e755039ff7af
Saul <smi0010@alu.ubu.es>
2020-03-24 20:08:42 +0100

    Añadidas fases de crecimiento
    Cuando se detectan un determinado número de colisiones, el contador se reinicia y se cambia el modelo 3D de la planta para representar un cambio de fase en su crecimiento.

--- Ejemplo/plant_test/Assets/Codigo/DetectWater.cs
+++ Ejemplo/plant_test/Assets/Codigo/DetectWater.cs
@@ -9,6 +9,12 @@ public class DetectWater : MonoBehaviour
     public List<ParticleCollisionEvent> collisionEvents;
     public static int agua = 0;
     public Text textoAgua;
+    public GameObject fase1;
+    public GameObject fase2;
+    public GameObject fase3;
+    public GameObject fase4;
+    public GameObject regadera;
+    bool flag = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,7 +45,35 @@ public class DetectWater : MonoBehaviour
     {
         //agua = agua + agua;
         textoAgua.text = "Agua: " + agua;
-
+        if (agua >= 5000)
+        {
+            agua = 0;
+            if (flag == true)
+            {
+                flag = false;
+                fase1.SetActive(true);
+            }
+            else if (fase1.active)
+            {
+                fase1.SetActive(false);
+                fase2.SetActive(true);
+                agua = 0;
+            }
+            else if (fase2.active)
+            {
+                fase2.SetActive(false);
+                fase3.SetActive(true);
+                agua = 0;
+            }
+            else if (fase3.active)
+            {
+                fase3.SetActive(false);
+                fase4.SetActive(true);
+                regadera.SetActive(false);
+                agua = 0;
+            }
+            
+        }
     }
     //// Update is called once per frame
     //void Update()

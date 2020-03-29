699bf54a4851e393ec5d8e546ec2c5f1e0251a57
Saul <smi0010@alu.ubu.es>
2020-03-30 00:31:56 +0200

    Añadida barra de progeso en interfaz
    Se ha añadido una barra de progreso en la interfaz, a medida que se completa el nivel se va rellenando. Al completarse el nivel se reinicia para el siguiente nivel.

--- Ejemplo/plant_test/Assets/Codigo/DetectWater.cs
+++ Ejemplo/plant_test/Assets/Codigo/DetectWater.cs
@@ -8,13 +8,20 @@ public class DetectWater : MonoBehaviour
     public ParticleSystem pSystem;
     public List<ParticleCollisionEvent> collisionEvents;
     public static int agua = 0;
+    public static float agua2;
+    float valPro;
+    float maxPro = 100;
+
     public Text textoAgua;
+    public Image barraProgreso;
+
     public GameObject fase1;
     public GameObject fase2;
     public GameObject fase3;
     public GameObject fase4;
     public GameObject regadera;
     bool flag = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +32,7 @@ public class DetectWater : MonoBehaviour
             Debug.LogError("No funciona jajaja", this);
             return;
         }
+        ActualizarProgreso(0);
         ActualizarHud();
     }
 
@@ -45,6 +53,8 @@ public class DetectWater : MonoBehaviour
     {
         //agua = agua + agua;
         textoAgua.text = "Agua: " + agua;
+        
+        ActualizarProgreso(agua);
         if (agua >= 5000)
         {
             agua = 0;
@@ -52,17 +62,20 @@ public class DetectWater : MonoBehaviour
             {
                 flag = false;
                 fase1.SetActive(true);
+                ActualizarProgreso(0);
             }
             else if (fase1.active)
             {
                 fase1.SetActive(false);
                 fase2.SetActive(true);
+                ActualizarProgreso(0);
                 agua = 0;
             }
             else if (fase2.active)
             {
                 fase2.SetActive(false);
                 fase3.SetActive(true);
+                ActualizarProgreso(0);
                 agua = 0;
             }
             else if (fase3.active)
@@ -70,10 +83,17 @@ public class DetectWater : MonoBehaviour
                 fase3.SetActive(false);
                 fase4.SetActive(true);
                 regadera.SetActive(false);
+                
                 agua = 0;
             }
             
         }
+    }
+    public void ActualizarProgreso(float amount)
+    {
+        agua2 = ((amount * maxPro) / 5000);
+        valPro = Mathf.Clamp(agua2, 0, maxPro);
+        barraProgreso.transform.localScale = new Vector2(valPro / maxPro, 1);
     }
     //// Update is called once per frame
     //void Update()

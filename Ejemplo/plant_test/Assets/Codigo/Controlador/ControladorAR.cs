5e7b9e2f08447943e2a46872afb03dcf249850ff
Saul <smi0010@alu.ubu.es>
2020-04-07 02:44:00 +0200

    Añadida plantilla ventana texto
    Se ha añadido una plantilla de una ventana emergente.

--- Ejemplo/plant_test/Assets/Codigo/Controlador/ControladorAR.cs
+++ Ejemplo/plant_test/Assets/Codigo/Controlador/ControladorAR.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
+
 public class ControladorAR : MonoBehaviour
 {
+    public GameObject regadera;
+    public GameObject ventama;
     // Start is called before the first frame update
+
+    public void Start()
+    {
+        regadera.SetActive(false);
+        ventama.SetActive(false);
+    }
+
     public void VolverMenu()
     {
         SceneManager.LoadScene("Menu");
     }
+
+    
+
+    
 }

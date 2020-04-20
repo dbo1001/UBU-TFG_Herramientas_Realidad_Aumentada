0c6b5f6518b93a50e928374c948e37f3062be9ca
Saul <smi0010@alu.ubu.es>
2020-04-21 01:40:32 +0200

    Añadido evento en la animación
    Se ha añadido un evento en la animación, con un script, elimina el objeto 3D una vez finalizada la animación.

---
+++ Ejemplo/plant_test/Assets/Codigo/Fertilizante.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fertilizante : MonoBehaviour
+{
+    public GameObject fertilizante;
+    //public AnimationClip animation;
+    //public Animator animator;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    fertilizante.anim
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+    
+    void EndAnimationEvent()
+    {
+        fertilizante.SetActive(false);
+    }
+}

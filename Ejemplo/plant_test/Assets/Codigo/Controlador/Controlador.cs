f14736e9a9b63e79862ba595b1df562ceef0727d
Saul <smi0010@alu.ubu.es>
2020-05-10 23:46:27 +0200

    Añadido Menú modo AR

--- Ejemplo/plant_test/Assets/Codigo/Controlador/Controlador.cs
+++ Ejemplo/plant_test/Assets/Codigo/Controlador/Controlador.cs
@@ -11,6 +11,13 @@ public class Controlador : MonoBehaviour
     public GameObject menu1_1_1;
     public GameObject atras;
 
+    public GameObject menu2;
+    public GameObject menu2_1;
+    public GameObject menu2_1_1;
+    public GameObject atras2;
+    public GameObject multiTarget;
+    public GameObject camaraAR;
+
 
 
     // Start is called before the first frame update
@@ -20,6 +27,7 @@ public class Controlador : MonoBehaviour
         SceneManager.LoadScene(scene);
     }
 
+    //Controla el botón Atras del menú.
     public void Atras()
     {
         if (!menu1.active)
@@ -37,12 +45,35 @@ public class Controlador : MonoBehaviour
         }
     }
 
+    //Controla el botón Atras del menú modo AR.
+    public void Atras2()
+    {
+        if (!menu2.active)
+        {
+            if (!menu2_1.active)
+            {
+                menu2_1_1.SetActive(false);
+                menu2_1.SetActive(true);
+            }
+            else
+            {
+                menu2_1.SetActive(false);
+                menu2.SetActive(true);
+                atras2.SetActive(false);
+            }
+        }
+    }
+
+
     public void Start()
     {
         menu1.SetActive(true);
         menu1_1.SetActive(false);
         menu1_1_1.SetActive(false);
         atras.SetActive(false);
+
+        camaraAR.SetActive(false);
+        multiTarget.SetActive(false);
 
     }
 }

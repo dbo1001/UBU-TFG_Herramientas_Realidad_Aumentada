a58dbf1803f3756a2efeb1648d3fbf9d96986f29
Saul <smi0010@alu.ubu.es>
2020-07-10 12:19:46 +0200

    Añadido modo con múltiples marcadores simultáneos

---
+++ Ejemplo/plant_test/Assets/Codigo/Controlador/ControladorMultiTarget.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.SceneManagement;
+
+public class ControladorMultiTarget : MonoBehaviour
+{
+    
+    public GameObject ImaDTarget;
+    public GameObject UDTBuilder;
+    public GameObject captura3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite))
+        {
+            Permission.RequestUserPermission(Permission.ExternalStorageWrite);
+        }
+#endif
+        ImaDTarget.SetActive(true);
+        UDTBuilder.SetActive(true);
+        captura3.SetActive(true);
+
+
+    }
+    
+    //Carga la scena del menú
+    public void VolverMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
